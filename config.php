9c3d2088fe8b671e612dc459496a7211187ff383
atcglub <107051678+droperos@users.noreply.github.com>
2022-06-11 09:57:53 +0700

    Update config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+{"Version":"1.7",
+
+"Url":"https://github.com/droperos/atcglub/raw/main/config.zip" ,
+
+"Changelog":"  
+อ้ฟเดทไฟล์ล่าสุด กดตกลง
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+ "-Demo"
+  ]
