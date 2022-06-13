58e7b451aa0b96ee222961b5d3ebf4dd7e9ae87c
atcglub <107051678+droperos@users.noreply.github.com>
2022-06-13 07:33:00 +0700

    Update combo.php

--- combo.php
+++ combo.php
@@ -1,23 +1,2 @@
-{"Version":"1.10",
-
-"Url":"https://github.com/droperos/atcglub/raw/main/combo.zip" ,
-
-"Changelog":"  
-
-⛔อัพเดทไฟล์ล่าสุด กด ตกลง⛔
-",
-
-"latestVersion": "1.0.0",
-
-"latestVersionCode": "1",
-
-"url": "",
-
- "releaseNotes": [
-
- "-Demo"
-
-  ]
-
-}
+1.5
 
