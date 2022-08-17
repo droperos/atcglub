e523fa810a35ca23889ada2753ee9bd16a296f05
atcglub <107051678+droperos@users.noreply.github.com>
2022-08-18 05:23:22 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,12 +3,7 @@
 "Url":"https://github.com/droperos/atcglub/raw/main/config.zip" ,
 
 "Changelog":"
-🆕 FREE × 3 unlimited
-🇸🇬 dtac + true nopro
-🆕 VIP × 1 premium 
-🇹🇭 ais play+64k
-🇹🇭 dtac all pro
-🇹🇭 true all pro
+🆕อัพเดทตอนนี้ กด ตกลง👇
 ",
 
 "latestVersion": "1.0.0",
